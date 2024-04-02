a110db906c918f0b34cb00b78aad7412895aff35
Caique Gomes <caiquegomesdev@gmail.com>
2024-04-01 22:35:36 -0300

    Padronização de slide de exercícios.

---
+++ Secao 05 - Conceitos Iniciais/kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Hello World!");
+    println("Ola mundo!");
+    //func main e o ponto de entrada
+
+    var nome = "Gomes";
+    //var nom: String = 123;
+    //var fun: String = "Gomes"
+    println(nome)
+}
+
+fun a2(){
+
+}
+
